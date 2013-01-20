2e105c6c29ed4a08ce42a23b74255baee7bb9075
Binoj D <dbinoj@gmail.com>
2013-01-20 18:48:15 +0530

    Initial commit to GIT.

---
+++ README.md
@@ -0,0 +1,12 @@
+*Guest Internet Access Card Generator*
+================================
+
+Customize options
+-----------------
+
+1. Edit index.php
+2. Change USERS_FILE definition to the path of the CSV in "username,password" format.
+   Line endings *LF*, UNIX format. Path relative to index.html
+3. Change PG_LAYOUT to the layout of the card. JPG of layout in A4 with 9 cards.
+4. Change PG_LAYOUT_X, PG_LAYOUT_Y according to the PG_LAYOUT file's dimensions.
+5. Visit index.php and print the cards!
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+require('./fpdf.php');
+define('USERS_FILE', 'users.csv');
+define('PG_LAYOUT', './guest-net-card-page-001.jpg');
+define('PG_LAYOUT_X','3506');
+define('PG_LAYOUT_Y','2478');
+
+$data = array();
+
+if ($fp = fopen(USERS_FILE, 'r')) {
+	$c=0;
+    while (!feof($fp)) {
+        $row = fgetcsv($fp);
+        $data[$c] = array($row[0],$row[1]);
+		$c++;
+    }
+
+    fclose($fp);
+}
+
+
+$pdf = new FPDF('L', 'pt', array(PG_LAYOUT_X, PG_LAYOUT_Y)); 
+
+$count = 0;
+foreach ($data as $leaf){
+if (($count+1)%9==1){
+
+$pdf->AddPage(); 
+
+$pdf->Image(PG_LAYOUT, 0, 0, 3506, 2478); 
+
+$pdf->SetFont('Courier', 'B', 45); 
+
+
+$pdf->Text(550, 520, $data[$count][0]);
+$pdf->Text(550, 575, $data[$count++][1]);
+	
+}
+elseif (($count+1)%9==2){
+$pdf->Text(1700, 520, $data[$count][0]);
+$pdf->Text(1700, 575, $data[$count++][1]);
+}
+elseif (($count+1)%9==3){
+$pdf->Text(2850, 520, $data[$count][0]);
+$pdf->Text(2850, 575, $data[$count++][1]);
+}
+elseif (($count+1)%9==4){
+$pdf->Text(550, 1330, $data[$count][0]);
+$pdf->Text(550, 1385, $data[$count++][1]);
+}
+elseif (($count+1)%9==5){
+$pdf->Text(1700, 1330, $data[$count][0]);
+$pdf->Text(1700, 1385, $data[$count++][1]);
+}
+elseif (($count+1)%9==6){
+$pdf->Text(2850, 1330, $data[$count][0]);
+$pdf->Text(2850, 1385, $data[$count++][1]);
+}
+elseif (($count+1)%9==7){
+$pdf->Text(550, 2140, $data[$count][0]);
+$pdf->Text(550, 2195, $data[$count++][1]);
+}
+elseif (($count+1)%9==8){
+$pdf->Text(1700, 2140, $data[$count][0]);
+$pdf->Text(1700, 2195, $data[$count++][1]);
+}
+elseif (($count+1)%9==0){
+$pdf->Text(2850, 2140, $data[$count][0]);
+$pdf->Text(2850, 2195, $data[$count++][1]);
+}
+}
+
+
+$pdf->Output();  
+
+
+?>
